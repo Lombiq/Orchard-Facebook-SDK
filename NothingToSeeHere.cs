f526d0320bf65bccb4b28edafb463d2f4f09d7cc
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2012-08-13 14:09:53 +0200

    Initial commit with SDK v. 6.0.20

---
+++ NothingToSeeHere.cs
@@ -0,0 +1,3 @@
+﻿// This empty file is only here to work around a bug when building the solution with
+// msbuild, see: http://stackoverflow.com/questions/986221/tfs-build-server-csc-fatal-error-cs2008-no-inputs-specified
+// Actually msbuild is also the cause of all the unused references added.
